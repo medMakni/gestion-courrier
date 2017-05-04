e2c4607511806c9e5989f7ef97adf6ab31ee50d3
wassim404 <wassim.makni@live.fr>
2017-05-04 16:45:57 +0100

    services

---
+++ src/main/java/biz/picosoft/services/MailService.java
@@ -0,0 +1,15 @@
+package biz.picosoft.services;
+
+import java.util.List;
+
+import org.apache.commons.fileupload.FileItem;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+    private final String UPLOAD_DIRECTORY = "C:/uploads";
+    public List<FileItem> putAttachment(){
+		return null;
+    	
+    }
+}
